26d84fd05690fa2ac1c7e2ff34f8c7a1bdf1699e
Massimo Paladin <massimo.paladin@gmail.com>
2015-10-05 11:39:57 +0200

    SONARJAVA-1288 Update the highlighting behaviour of rule S133 "Methods should not be too complex"

--- java-checks/src/test/files/checks/MethodComplexityNoncompliant.java
+++ java-checks/src/test/files/checks/MethodComplexityNoncompliant.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-  public void sayHello() { // Noncompliant [[effortToFix=1]] {{The Cyclomatic Complexity of this method "sayHello" is 2 which is greater than 1 authorized.}}
+  public void sayHello() { // Noncompliant [[effortToFix=1;sc=15;ec=23;secondary=3,4]] {{The Cyclomatic Complexity of this method "sayHello" is 2 which is greater than 1 authorized.}}
     while (false) {
     }
   }
